6d026e1f13d557c8621ec9d4f3b12bd7d5692b86
Spencer <spencerwong24@gmail.com>
2019-09-18 16:34:22 -0700

    setup server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000")
+});
