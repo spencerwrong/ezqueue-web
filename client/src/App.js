4d2669491cfd79771084d008089f6923b4527158
Sterling <sterlingsgamble@gmail.com>
2019-11-23 10:14:02 -0800

    bootstrap + react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+import Dashboard from "./components/dashboard/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
+          {/* <Route path="/profile/:id" component={Profile} /> */}
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
